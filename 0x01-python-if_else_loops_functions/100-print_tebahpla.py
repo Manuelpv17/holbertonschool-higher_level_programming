b6855a8503b085189fcf935b29b9b51e53e0f84b
Manuelpv17 <manuelpv17@outlook.com>
2020-04-28 14:41:17 -0500

    Add new logic

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-print("zYxWvUtSrQpOnMlKjIhGfEdCbA")
+for i in range(122, 96, -1):
+    if i % 2 == 0:
+        print("{}".format(chr(i)), end='')
+    else:
+        print("{}".format(chr(i - 32)), end='')
