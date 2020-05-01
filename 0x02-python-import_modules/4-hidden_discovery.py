f759fc3f5cfa19413dc8f7643b3344edc1cb6429
Manuelpv17 <manuelpv17@outlook.com>
2020-04-30 21:51:14 -0500

    Fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import sys
-import hidden_4 as hidden
-if __name__ != "__main__":
-    exit()
-for name in dir(hidden):
-    if name[0:2] != "__":
-        print(name)
+import hidden_4
+if __name__ == "__main__":
+    for name in dir(hidden_4):
+        if name[0:2] != "__":
+            print(name)
