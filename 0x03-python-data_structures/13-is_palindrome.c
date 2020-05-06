74d9cc49bea32de091ea45c6d3bce28be9dfae9e
Manuelpv17 <manuelpv17@outlook.com>
2020-05-05 21:06:18 -0500

    Add task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,7 @@
 #include "lists.h"
 
+int aux_func(listint_t **, listint_t *);
+
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: head of list
@@ -7,26 +9,17 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *back_head = NULL;
-	listint_t *aux_head = NULL;
-	int len, i, j;
+	return (aux_func(head, *head));
+}
 
-	if (*head == NULL)
-		return (1);
-
-	back_head = *head;
-	for (len = 0; back_head->next != NULL; len++)
-		back_head = back_head->next;
-	aux_head = *head;
-	for (i = 0; i < (len / 2) + 1; i++)
+int aux_func(listint_t **head, listint_t *b_head)
+{
+	if (b_head->next != NULL)
+		aux_func(head, b_head->next);
+	if ((*head)->n == b_head->n)
 	{
-		if (aux_head->n != back_head->n)
-			return (0);
-
-		back_head = *head;
-		for (j = 0; j < len - i - 1; j++)
-			back_head = back_head->next;
-		aux_head = aux_head->next;
+		*head = (*head)->next;
+		return (1);
 	}
-	return (1);
+	return (0);
 }
