03d486136ed8a032a12d883e580dd07d42bff633
Manuelpv17 <manuelpv17@outlook.com>
2020-05-07 21:24:07 -0500

    Add None scenario

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,13 @@
 def roman_to_int(roman_string):
     number = 0
     roman = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
-    for letter in roman_string:
-        if letter in roman:
-            if number > 0 and roman[letter] > number:
-                number = roman[letter] - number
+    if roman_string:
+        for letter in roman_string:
+            if letter in roman:
+                if number > 0 and roman[letter] > number:
+                    number = roman[letter] - number
+                else:
+                    number += roman[letter]
             else:
-                number += roman[letter]
-        else:
-            return(0)
+                return(0)
     return(number)
