62554de33836db42a05d11f1ec4b6769a1e97d2d
Manuelpv17 <manuelpv17@outlook.com>
2020-05-07 20:02:04 -0500

    Improve to python power

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,11 +3,7 @@ def uniq_add(my_list=[]):
     sum = 0
     new = []
     for item in my_list:
-        flag = 0
-        for new_item in new:
-            if new_item == item:
-                flag = 1
-        if flag == 0:
+        if item not in new:
             new.append(item)
             sum += item
     return(sum)
