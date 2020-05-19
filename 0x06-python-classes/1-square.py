db64fe945051274077c57914d6d21351f3b95e77
Manuelpv17 <manuelpv17@outlook.com>
2020-05-19 18:29:53 -0500

    Add task 1, Square with Size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""File for defining Class Square"""
+
+
+class Square:
+    """class Square that defines a square """
+
+    def __init__(self, size):
+        Square.__size = size
