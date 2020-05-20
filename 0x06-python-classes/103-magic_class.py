7016dd5767d7668f67dde2823f44867a93173e01
Manuelpv17 <manuelpv17@outlook.com>
2020-05-19 23:37:00 -0500

    Change name of radius

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -7,10 +7,10 @@ class _MagicClass__radius:
         if type(radius) is not int or type(radius) is not float:
             raise TypeError('radius must be a number')
         else:
-            self.radius = radius
+            self._MagicClass__radius = radius
 
     def area(self):
-        return (self.radius ** 2) * math.pi
+        return (self._MagicClass__radius ** 2) * math.pi
 
     def circumference(self):
-        return 2 * math.pi * self.radius
+        return 2 * math.pi * self._MagicClass__radius
