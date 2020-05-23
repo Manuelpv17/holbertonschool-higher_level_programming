541c99cd15d1656c2c963b4dde3ffa16baa6bb3c
Manuelpv17 <manuelpv17@outlook.com>
2020-05-22 20:21:44 -0500

    Add task 2 divide a matrix

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    new = []
+    if type(div) not in [int, float]:
+        raise TypeError(
+            "div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    if type(matrix) is not list:
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+
+    size = len(matrix[0])
+    for row in matrix:
+        new_row = []
+        if type(row) is not list:
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+        if size != len(row):
+            raise TypeError("Each row of the matrix must have the same size")
+        for item in row:
+            if type(item) not in [int, float]:
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of integers/floats")
+            new_row.append(round(item / div, 2))
+        new.append(new_row)
+    return new
