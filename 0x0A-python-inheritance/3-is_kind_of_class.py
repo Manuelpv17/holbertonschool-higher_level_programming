3bea340b293247d745350fb3213b1f1d714e960a
Manuelpv17 <manuelpv17@outlook.com>
2020-06-02 19:55:17 -0500

    Add task 3 kind of class

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+"""Same class or inherit from
+    """
+
+
 def is_kind_of_class(obj, a_class):
+    """object is an instance of, or if the object is an instance of a class that inherited from
+
+        Arguments:
+                obj {[type]} -- Object
+                a_class {[type]} -- Class
+
+        Returns:
+                Bool -- True if instance
+        """
     return isinstance(obj, a_class)
