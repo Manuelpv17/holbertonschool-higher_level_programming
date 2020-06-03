37779e95f1ed2774944e0394cf42b7820ad40648
Manuelpv17 <manuelpv17@outlook.com>
2020-06-02 23:08:23 -0500

    Add task 101 add_attribute

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function that adds a new attribute to an
+object if it’s possible
+    """
+
+
+def add_attribute(obj, att, value):
+    if "__dict__" not in dir(obj):
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, att, value)
