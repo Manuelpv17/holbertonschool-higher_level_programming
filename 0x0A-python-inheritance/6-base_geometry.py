472c104e3667967697bf4524cf5c9bb50144766b
Manuelpv17 <manuelpv17@outlook.com>
2020-06-02 20:05:02 -0500

    Add task 6 Improve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Geometry module
+    """
+
+
+class BaseGeometry:
+    """empty class BaseGeometry
+        """
+
+    def area(self):
+        raise Exception("area() is not implemented")
