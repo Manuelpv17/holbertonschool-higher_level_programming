6fb48975d314d1c751b20d171c756938c0652bcf
Manuelpv17 <manuelpv17@outlook.com>
2020-06-02 19:59:03 -0500

    Add docstring task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
+"""Only sub class of
+    """
+
+
 def inherits_from(obj, a_class):
+    """if the object is an instance of a class
+    that inherited (directly or indirectly)
+    from the specified class ; otherwis
+    False.
+
+        Arguments:
+                obj {[type]} -- Object
+                a_class {[type]} -- Class
+
+        Returns:
+                Bool -- True if inherits
+        """
     return isinstance(obj, a_class) and type(obj) is not a_class
