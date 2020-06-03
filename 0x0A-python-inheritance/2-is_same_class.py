2a9f275504d9f5b2ebb6ddf21da7669978e66972
Manuelpv17 <manuelpv17@outlook.com>
2020-06-02 19:51:11 -0500

    Add docstrings task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+"""Exact same object
+    """
+
+
 def is_same_class(obj, a_class):
+    """CHeck exact same object
+
+        Arguments:
+                obj {[type]} -- Object
+                a_class {[type]} -- Class
+
+        Returns:
+                Boolean -- True if exact same
+        """
     return type(obj) is a_class
