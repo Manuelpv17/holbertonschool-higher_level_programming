4624228ab8f4de403d33ad64b673ae036a2594f5
Manuelpv17 <manuelpv17@outlook.com>
2020-06-02 22:30:09 -0500

    Add task 12 my_integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""My integer
+    """
+
+
+class MyInt(int):
+
+    def __init__(self, num):
+        self.num = num
+
+    def __eq__(self, other):
+        return self.num != other
+
+    def __ne__(self, other):
+        return self.num == other
