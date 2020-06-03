e930e8352c29388b510f89bad926baa88bd3ede5
Manuelpv17 <manuelpv17@outlook.com>
2020-06-02 19:26:22 -0500

    Add More Docstring to task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""returns the list of available attributes and methods of an object
+"""function that returns the list of available attributes and methods
+of an object
     """
 
 
 def lookup(obj):
+    """function that returns the list of available attributes and
+    methods of an object
+        """
     return dir(obj)
