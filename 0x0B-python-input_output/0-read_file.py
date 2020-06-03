59952cedb6b36f5a967271e7f5f08b8f93c27611
Manuelpv17 <manuelpv17@outlook.com>
2020-06-03 18:08:37 -0500

    Add docstriong for task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""Read file
+    """
+
+
 def read_file(filename=""):
+    """ reads a text file (UTF8) and
+    prints it to stdou
+
+        Keyword Arguments:
+                filename {str} -- file with text (default: {""})
+        """
     with open(filename, encoding="utf-8") as m:
         print(m.read())
