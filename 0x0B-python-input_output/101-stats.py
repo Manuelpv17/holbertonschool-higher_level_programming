51f95286ecab570434978559bdbf091982b6ceec
Manuelpv17 <manuelpv17@outlook.com>
2020-06-04 16:33:54 -0500

    Add task log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -9,24 +9,19 @@ status = {'200': 0, '301': 0, '400': 0, '401': 0,
 cont = 0
 size = 0
 total_size = 0
-try:
-    for line in sys.stdin:
-        cont += 1
-        w = line.split()
-        status[w[-2]] += 1
 
-        total_size += int(w[-1])
-        if cont % 10 == 0:
-            print("File size: {}".format(total_size))
-            for stat_key, stat_value in status.items():
-                if stat_value > 0:
-                    print("{}: {}".format(stat_key, stat_value))
-    if cont % 10 != 0:
+for line in sys.stdin:
+    cont += 1
+    w = line.split()
+    status[w[-2]] += 1
+    total_size += int(w[-1])
+    if cont % 10 == 0:
         print("File size: {}".format(total_size))
         for stat_key, stat_value in status.items():
             if stat_value > 0:
                 print("{}: {}".format(stat_key, stat_value))
-except Exception:
+
+if cont % 10 != 0 or cont == 0:
     print("File size: {}".format(total_size))
     for stat_key, stat_value in status.items():
         if stat_value > 0:
