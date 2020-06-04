d61f45379471ec4e7107df67eebc1a0588673d37
Manuelpv17 <manuelpv17@outlook.com>
2020-06-03 19:38:01 -0500

    Add task 9

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Load, add, save 
+    """
+from sys import argv
+import json
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+try:
+    prev = load_from_json_file("add_item.json")
+except:
+    prev = []
+for elem in argv[1:]:
+    prev.append(elem)
+save_to_json_file(prev, "add_item.json")
