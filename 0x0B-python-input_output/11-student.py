03c1a43498dda74a4f1520cf41ed5cc546644928
Manuelpv17 <manuelpv17@outlook.com>
2020-06-03 20:00:21 -0500

    Corrected docstring

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -4,8 +4,9 @@
 
 
 class Student:
-	"""class Student that defines a student
-	"""
+    """class Student that defines a student
+        """
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
