fd041aba9da2a39d852f0533ac70625c6472e887
Manuelpv17 <manuelpv17@outlook.com>
2020-06-03 20:13:51 -0500

    Add task 12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Student to JSON
+    """
+
+
+class Student:
+    """class Student that defines a student
+        """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        new_dict = {}
+        if type(attrs) is not list:
+            return self.__dict__
+        for key, val in self.__dict__.items():
+            if key in attrs:
+                new_dict[key] = val
+        return new_dict
