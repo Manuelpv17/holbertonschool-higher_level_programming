43f35624b3150352453200938e9c5456434e783e
Manuelpv17 <manuelpv17@outlook.com>
2020-06-04 19:48:26 -0500

    Add task log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,6 +3,14 @@
     """
 import sys
 
+
+def print_format():
+    print("File size: {}".format(total_size))
+    for stat_key, stat_value in status.items():
+        if stat_value > 0:
+            print("{}: {}".format(stat_key, stat_value))
+
+
 status = {'200': 0, '301': 0, '400': 0, '401': 0,
           '403': 0, '404': 0, '405': 0, '500': 0}
 
@@ -10,19 +18,18 @@ cont = 0
 size = 0
 total_size = 0
 
-for line in sys.stdin:
-    cont += 1
-    w = line.split()
-    status[w[-2]] += 1
-    total_size += int(w[-1])
-    if cont % 10 == 0:
-        print("File size: {}".format(total_size))
-        for stat_key, stat_value in status.items():
-            if stat_value > 0:
-                print("{}: {}".format(stat_key, stat_value))
+try:
+    for line in sys.stdin:
+        cont += 1
+        w = line.split()
+        status[w[-2]] += 1
+        total_size += int(w[-1])
+        if cont % 10 == 0:
+            print_format()
 
-if cont % 10 != 0 or cont == 0:
-    print("File size: {}".format(total_size))
-    for stat_key, stat_value in status.items():
-        if stat_value > 0:
-            print("{}: {}".format(stat_key, stat_value))
+    if cont % 10 != 0 or cont == 0:
+        print_format()
+
+except KeyboardInterrupt:
+    print_format()
+    raise
