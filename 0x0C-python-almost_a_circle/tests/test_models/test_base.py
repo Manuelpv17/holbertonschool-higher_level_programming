d71b7f07f9005fb2c04cc0bb6e219264a7181e17
Manuelpv17 <manuelpv17@outlook.com>
2020-06-09 15:26:04 -0500

    Add more checks

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -9,10 +9,16 @@ from models.square import Square
 class TestBase(unittest.TestCase):
     """Unittest"""
 
-    def test_Base(self):
+    def test_base_class(self):
         """Test base"""
         self.assertEqual(Base().id, 1)
-        self.assertEqual(Base().id, 2)
-        self.assertEqual(Base().id, 3)
         self.assertEqual(Base(12).id, 12)
-        self.assertEqual(Base().id, 4)
+        self.assertEqual(Base().id, 2)
+
+    def test_first_rectangle(self):
+        r = Rectangle(10, 2)
+        self.assertEqual(r.id, 3)
+        r1 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r1.id, 12)
+        r2 = Rectangle(10, 2)
+        self.assertEqual(r2.id, 4)
