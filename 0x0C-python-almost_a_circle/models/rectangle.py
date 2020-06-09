a09292b250e84dfd8dc54f38fd9fb2e854f0e615
Manuelpv17 <manuelpv17@outlook.com>
2020-06-09 12:21:35 -0500

    Add docstring

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-
+""" Rectangle 
+"""
 from models.base import Base
 
 
 class Rectangle(Base):
+    """ class Rectangle that inherits from Base
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
