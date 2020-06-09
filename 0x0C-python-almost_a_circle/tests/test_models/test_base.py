fbb170098db88d9a3143faab2342b19bd438bf6b
Manuelpv17 <manuelpv17@outlook.com>
2020-06-09 15:06:40 -0500

    Initial test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,12 +2,14 @@
 """tests!"""
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
     """Unittest"""
 
-    def testBase(self):
+    def test_Base(self):
         self.assertEqual(Base().id, 1)
         self.assertEqual(Base().id, 2)
         self.assertEqual(Base().id, 3)
