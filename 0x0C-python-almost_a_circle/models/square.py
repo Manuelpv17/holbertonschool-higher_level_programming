4fa39f768aa35cf2c464dc959fe81cd96ad69df9
Manuelpv17 <manuelpv17@outlook.com>
2020-06-09 12:46:41 -0500

    Add docstring

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """class Square that inherits from Rectangle"""
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """Square class"""
 
     def __init__(self, size, x=0, y=0, id=None):
         """init"""
