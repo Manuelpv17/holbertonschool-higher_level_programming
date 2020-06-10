3bbdf64610a8393a7c676d1be7ce20362390617d
Manuelpv17 <manuelpv17@outlook.com>
2020-06-09 21:07:32 -0500

    MORE TEST PEP8 UNIT TEST

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -13,11 +13,11 @@ class TestBase(unittest.TestCase):
     def test_pep8_conformance(self):
         pep8style = pep8.StyleGuide(quiet=True)
         b = pep8style.check_files(['models/base.py'])
-        s = pep8style.check_files(['models/square.py'])
-        r = pep8style.check_files(['models/rectangle.py'])
         self.assertEqual(
             b.total_errors, 0, "Found code style errors (and warnings).")
-        self.assertEqual(
-            s.total_errors, 0, "Found code style errors (and warnings).")
+        r = pep8style.check_files(['models/rectangle.py'])
         self.assertEqual(
             r.total_errors, 0, "Found code style errors (and warnings).")
+        s = pep8style.check_files(['models/square.py'])
+        self.assertEqual(
+            s.total_errors, 0, "Found code style errors (and warnings).")
