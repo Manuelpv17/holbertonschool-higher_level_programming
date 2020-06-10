536a065c240575e996269274702a6b8bfa5dee14
Manuelpv17 <manuelpv17@outlook.com>
2020-06-09 20:29:43 -0500

    Add more test cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -20,3 +20,13 @@ class TestBase(unittest.TestCase):
         """Tears down obj count
         """
         Base._Base__nb_objects = 0
+
+    def test_dict_to_json(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertEqual(
+            dictionary, {'x': 2, 'width': 10, 'id': 1, 'height': 7, 'y': 8})
+        self.assertEqual(type(dictionary), dict)
+        self.assertEqual(json_dictionary, "[{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}]")
+        self.assertEqual(type(json_dictionary), str)
