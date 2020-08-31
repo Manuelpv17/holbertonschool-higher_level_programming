3708dbd06e8d132da2634e28af7351b6bb72f336
Manuelpv17 <manuelpv17@outlook.com>
2020-08-30 21:12:03 -0500

    Add task 2 more filters

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -21,7 +21,8 @@ def filter_func():
 
     cursor = db.cursor()
     cursor.execute(
-        "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY states.id ASC;".format(
+        "SELECT * FROM states WHERE BINARY\
+             name = '{}' ORDER BY states.id ASC;".format(
             sys.argv[4]))
 
     r = cursor.fetchall()
