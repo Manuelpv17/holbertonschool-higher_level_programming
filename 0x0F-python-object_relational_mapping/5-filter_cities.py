3e73098a6b0802fdd0fbadd8588e6a523be06f58
Manuelpv17 <manuelpv17@outlook.com>
2020-08-30 21:59:19 -0500

    Add task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""takes in the name of a state as an argument and lists 
+"""takes in the name of a state as an argument and lists
 all cities of that state, using the database hbtn_0e_4_usa"""
 
 import MySQLdb
