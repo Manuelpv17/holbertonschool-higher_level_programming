23fcca4a5950a195434d413667279cd8dd28a8a5
Manuelpv17 <manuelpv17@outlook.com>
2020-08-31 11:25:39 -0500

    Add task 15 relationship

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""creates the State “California” with the City “San Francisco” from the database hbtn_0e_100_usa"""
+"""creates the State “California” with the City “San Francisco”
+from the database hbtn_0e_100_usa"""
 
 from sys import argv
 from sqlalchemy import create_engine
