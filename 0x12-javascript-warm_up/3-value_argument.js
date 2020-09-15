ae3b22c7ff43723e3de322564fd69b6223ebc4fd
Manuelpv17 <manuelpv17@outlook.com>
2020-09-15 12:14:48 -0500

    Correction, no length

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
-const length = process.argv.length - 2;
-if (length === 0) {
+if (isNaN(process.argv[2])) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
