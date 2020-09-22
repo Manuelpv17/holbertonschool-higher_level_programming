5280f6f760cda4d94dda8eec5dcdef962ffe775e
Manuelpv17 <manuelpv17@outlook.com>
2020-09-22 00:07:34 -0500

    Asynchronously read/write

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,14 +4,11 @@ const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', function (err, data1) {
   if (err) {
-    return console.log(err);
   }
   fs.readFile(process.argv[3], 'utf8', function (err, data2) {
     if (err) {
-      return console.log(err);
     }
     fs.writeFile(process.argv[4], data1 + data2, 'utf8', function (err) {
-      return console.log(err);
     });
   });
 });
