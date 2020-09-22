ad79a688d0dd3636e55f7f729c24db80e31cbc5c
Manuelpv17 <manuelpv17@outlook.com>
2020-09-22 14:54:06 -0500

    Add task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  fs.writeFile(file, body, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
