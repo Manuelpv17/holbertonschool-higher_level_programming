bbc0747c094197e0c1f76d2f9bb7609270106610
Manuelpv17 <manuelpv17@outlook.com>
2020-09-25 09:02:43 -0500

    Semistandard styly fix

--- 0x14-javascript-web_scraping/102-search_twitter.js
+++ 0x14-javascript-web_scraping/102-search_twitter.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const request = require("request");
-const base64 = require("base-64");
-const utf8 = require("utf8");
+const request = require('request');
+const base64 = require('base-64');
+const utf8 = require('utf8');
 
 const consumerKey = process.argv[2];
 const consumerSecret = process.argv[3];
@@ -13,20 +13,20 @@ const encodeKey = base64.encode(
 
 request.post(
   {
-    url: "https://api.twitter.com/oauth2/token?grant_type=client_credentials",
+    url: 'https://api.twitter.com/oauth2/token?grant_type=client_credentials',
     headers: {
       Authorization: `Basic ${encodeKey}`,
-      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-    },
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    }
   },
   function (error, response, body) {
     if (!error) {
       const token = JSON.parse(body).access_token;
       request.get(
         {
-          url: "https://api.twitter.com/1.1/search/tweets.json",
-          headers: { Authorization: "Bearer " + token },
-          qs: { q: process.argv[4], count: "5" },
+          url: 'https://api.twitter.com/1.1/search/tweets.json',
+          headers: { Authorization: 'Bearer ' + token },
+          qs: { q: process.argv[4], count: '5' }
         },
         function (error, response, body) {
           if (!error) {
