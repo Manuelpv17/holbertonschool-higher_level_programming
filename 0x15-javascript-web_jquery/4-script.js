135f688b7e66fa59d517e8b0915fa740735e3b66
Manuelpv17 <manuelpv17@outlook.com>
2020-09-26 20:15:02 -0500

    Add new jQuery Logic

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,9 +1,4 @@
 $("DIV#toggle_header").click(function () {
-  if ($("header").hasClass("green")) {
-    $("header").removeClass("green");
-    $("header").addClass("red");
-  } else {
-    $("header").removeClass("red");
-    $("header").addClass("green");
-  }
+  $("header").toggleClass("green");
+  $("header").toggleClass("red");
 });
