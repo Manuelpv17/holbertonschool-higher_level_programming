5dab71acb6b2c1c7d87d2059c573c5b107f44b8b
Manuelpv17 <manuelpv17@outlook.com>
2020-10-16 08:32:12 -0500

    New logic recursion

--- 0x10-python-network_0/6-main.py
+++ 0x10-python-network_0/6-main.py
@@ -2,9 +2,23 @@
 """ Test function find_peak """
 find_peak = __import__('6-peak').find_peak
 
-print(find_peak([1, 2, 4, 6, 3]))
-print(find_peak([4, 2, 1, 2, 3, 1]))
-print(find_peak([2, 2, 2]))
-print(find_peak([]))
-print(find_peak([-2, -4, 2, 1]))
-print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
+a = [1, 2, 4, 6, 3]
+print(find_peak(a), a)
+a = [4, 2, 1, 2, 3, 1]
+print(find_peak(a), a)
+a = [2, 2, 2]
+print(find_peak(a), a)
+a = []
+print(find_peak(a), a)
+a = [-2, -4, 2, 1]
+print(find_peak(a), a)
+a = [4, 2, 1, 2, 2, 2, 3, 1]
+print(find_peak(a), a)
+a = [1, 2, 3, 4, 5]
+print(find_peak(a), a)
+a = [5, 4, 3, 2, 1]
+print(find_peak(a), a)
+a = [1, 4, 6, 2, 1]
+print(find_peak(a), a)
+a = [5, 4, 6, 2, 1, 4, 5, 2]
+print(find_peak(a), a)
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,15 +5,18 @@
 def find_peak(list_of_integers):
     """ Find a peak """
     lenList = len(list_of_integers)
-    right = lenList - 1
-    left = 0
-    if (lenList == 0):
+    if lenList == 0:
         return None
+    return recursionPeak(list_of_integers, 0, lenList - 1)
 
-    while (right > left):
-        mid = (right + 1 + left) // 2
-        if list_of_integers[mid - 1] > list_of_integers[mid]:
-            right = mid - 1
-        else:
-            left = mid
-    return left
+
+def recursionPeak(intList, left, right):
+    if (left == right):
+        return intList[left]
+    mid = (left + right) // 2
+    if (intList[mid] > intList[mid + 1]) and (mid == left or intList[mid] > intList[mid - 1]):
+        return intList[mid]
+    elif intList[mid] < intList[mid + 1]:
+        return recursionPeak(intList, mid + 1, right)
+    else:
+        return recursionPeak(intList, left, mid - 1)
