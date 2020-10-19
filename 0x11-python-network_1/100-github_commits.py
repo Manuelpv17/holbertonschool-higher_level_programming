1a839c488a547982658c2c0abc4e4fa5226c06b6
Manuelpv17 <manuelpv17@outlook.com>
2020-10-19 08:26:27 -0500

    Add task 10 GitHub challenge

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Time for an interview! Challenge """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    repName = sys.argv[1]
+    ownerName = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        ownerName, repName)
+    r = requests.get(url)
+    body = r.json()
+    for i in range(10):
+        print("{}: {}".format(body[i]["sha"],
+                              body[i]["commit"]["author"]["name"]))
